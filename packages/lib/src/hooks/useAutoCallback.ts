3ee3e7152c0de58d0d8fbc41e2e10244926da52f
김수현 <kimsuhyeon@gimsuhyeon-ui-MacBookPro.local>
2025-07-21 23:00:03 +0900

    useAutoCallback 구현

--- packages/lib/src/hooks/useAutoCallback.ts
+++ packages/lib/src/hooks/useAutoCallback.ts
@@ -1,7 +1,10 @@
 import type { AnyFunction } from "../types";
 import { useCallback } from "./useCallback";
 import { useRef } from "./useRef";
-
+//함수 타입만 받아서 함수 그대로 반환하는 구조
 export const useAutoCallback = <T extends AnyFunction>(fn: T): T => {
-  return fn;
+  const ref = useRef(fn);
+  ref.current = fn;
+  //(...args: Parameters<T>) ...args는 fn의 매개변수 타입을 그대로 가져옴
+  return useCallback((...args: Parameters<T>) => ref.current(...args), []) as T;
 };
