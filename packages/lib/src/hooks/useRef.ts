c059409984f99cdbdfca12de77bd493d184d4f22
김수현 <kimsuhyeon@gimsuhyeon-ui-MacBookPro.local>
2025-07-21 16:10:19 +0900

    useRef 구현

--- packages/lib/src/hooks/useRef.ts
+++ packages/lib/src/hooks/useRef.ts
@@ -1,4 +1,11 @@
+import { useState } from "react";
+
 export function useRef<T>(initialValue: T): { current: T } {
-  // useState를 이용해서 만들어보세요.
-  return { current: initialValue };
+  //반환 타입이 {current : T}
+  /**
+   * useState를 사용하여 초기값을 가진 ref object을 생성
+   * ref.current 값을 직접 바궈도 리렌더링 되지 않음
+   **/
+  const [ref] = useState<{ current: T }>({ current: initialValue });
+  return ref;
 }
