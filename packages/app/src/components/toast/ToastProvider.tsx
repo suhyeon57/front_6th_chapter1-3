bb1732266d7f6f2a9209805158c6419c9c4bfeaf
김수현 <kimsuhyeon@gimsuhyeon-ui-MacBookPro.local>
2025-07-23 14:35:52 +0900

    ToastProvider 구현

--- packages/app/src/components/toast/ToastProvider.tsx
+++ packages/app/src/components/toast/ToastProvider.tsx
@@ -17,13 +17,7 @@ import { debounce } from "../../utils";
 type ShowToast = (message: string, type: ToastType) => void;
 type Hide = () => void;
 
-const ToastStateContext = createContext<{
-  message: string;
-  type: ToastType;
-}>({
-  ...initialState,
-});
-
+//action context 설정
 const ToastActionsContext = createContext<{
   show: ShowToast;
   hide: Hide;
@@ -32,8 +26,17 @@ const ToastActionsContext = createContext<{
   hide: () => null,
 });
 
+//state context 설정
+const ToastStateContext = createContext<{
+  message: string;
+  type: ToastType;
+}>({
+  ...initialState,
+});
+
 const DEFAULT_DELAY = 3000;
 
+//커스텀 훅 : 자주 사용하는 로직을 커스텀 훅으로 묶음(외부에서도 사용 가능)
 export const useToastCommand = () => {
   const { show, hide } = useContext(ToastActionsContext);
   return { show, hide };
@@ -46,7 +49,7 @@ export const useToastState = () => {
 
 export const ToastProvider = memo(({ children }: PropsWithChildren) => {
   const [state, dispatch] = useReducer(toastReducer, initialState);
-  const { show, hide } = useRef(createActions(dispatch)).current;
+  const { show, hide } = useRef(createActions(dispatch)).current; //useRef를 사용하였기 때문에 리렌더링과 무관하게 고정된 값 유지
   const visible = state.message !== "";
 
   const hideAfter = debounce(hide, DEFAULT_DELAY);
@@ -54,9 +57,9 @@ export const ToastProvider = memo(({ children }: PropsWithChildren) => {
   const showWithHide: ShowToast = useCallback((...args) => {
     show(...args);
     hideAfter();
-  }, []);
+  }, []); //의존성 배열을 빈 배열로 설정하여 최초 렌더링 시에만 실행되도록 함
 
-  const context = useMemo(() => ({ show: showWithHide, hide }), [showWithHide, hide]);
+  const context = useMemo(() => ({ show: showWithHide, hide }), [showWithHide, hide]); //showWithHide와 hide가 변하지 않으므로 변하지 않음
 
   return (
     <ToastActionsContext value={context}>
